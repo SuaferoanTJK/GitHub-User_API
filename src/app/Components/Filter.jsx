eb7de8c9cd3c791717bee4ee761f3f6b4cd5db77
SuaferoanTJK <suarezroanfelipe@gmail.com>
2022-04-24 12:38:41 -0500

    Filter design and utility for dark added

--- src/app/Components/Filter.jsx
+++ src/app/Components/Filter.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Icons } from "./Icons";
 
 const Filter = () => {
-  return <div>Filter</div>;
+  return (
+    <div className="filter">
+      <button
+        className="filter_icon"
+        onClick={() => {
+          console.log("Icon search clicked");
+        }}
+      >
+        <img src={Icons.Search} alt="Search Icon" />
+      </button>
+      <input
+        className="filter_input"
+        type="text"
+        placeholder="Search GitHub username..."
+        onChange={(search) => {
+          console.log(search);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            console.log("Enter clicked");
+          }
+        }}
+      />
+      <button
+        className="filter_btn"
+        onClick={() => {
+          console.log("Btn search clicked");
+        }}
+      >
+        Search
+      </button>
+    </div>
+  );
 };
 
 export default Filter;
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../Components/Header";
+import Filter from "../Components/Filter";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="container">
-        <Header />
+    <div className="page">
+      <div className="home">
+        <div className="container">
+          <Header />
+          <Filter />
+        </div>
       </div>
     </div>
   );
